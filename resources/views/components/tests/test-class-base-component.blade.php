e9dd4e88f3ef8f9a55baa3130a2ab64d3e0cd743
takashi_abe <danee_dance@icloud.com>
2023-06-07 13:32:53 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+</div>
