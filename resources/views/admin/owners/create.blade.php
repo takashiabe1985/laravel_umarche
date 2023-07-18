f7233b800d7491d4624ab62ddce21c1357825e39
takashi_abe <takashi.abe@synergy-effect.jp>
2023-07-18 22:56:36 +0900

    リサイズ無しのアップロード

--- resources/views/admin/owners/create.blade.php
+++ resources/views/admin/owners/create.blade.php
@@ -9,7 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    
                     <section class="text-gray-600 body-font relative">
                         <div class="container px-5  mx-auto">
                           <div class="flex flex-col text-center w-full mb-12">
