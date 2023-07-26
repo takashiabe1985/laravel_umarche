802f8e0862896d2dd07cc81d1654a894c2c9509d
takashi_abe <takashi.abe@synergy-effect.jp>
2023-07-26 11:50:16 +0900

    Constant

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Log;
 use Throwable;
 use App\Http\Requests\ProductRequest;
 
+
 class ProductController extends Controller
 {
     public function __construct()
@@ -174,10 +175,10 @@ class ProductController extends Controller
                         $product->is_selling = $request->is_selling;
                         $product->save();
                     
-                    if($request->type === '1') {
+                    if($request->type === 'Constant'::PRODUCT_LIST['add']) {
                         $newQuantity = $request->quantity;
                     }
-                    if($request->type === '2') {
+                    if($request->type === 'Constant'::PRODUCT_LIST['reduce']) {
                         $newQuantity = $request->quantity * -1;
                     }
                     Stock::create([
