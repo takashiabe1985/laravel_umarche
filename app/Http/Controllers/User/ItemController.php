c3b97d665a80c5e4539a8d7a724f1c36ab89d720
takashi_abe <takashi.abe@synergy-effect.jp>
2023-07-27 10:35:57 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,6 +9,11 @@ use App\Models\Product;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+    
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -40,5 +45,12 @@ class ItemController extends Controller
         // $products = Product::all();
 
         return view('user.index', compact('products'));
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
     }
 }
