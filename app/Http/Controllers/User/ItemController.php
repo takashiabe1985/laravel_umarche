420888955d146e3761e89f308f62b1c8d94d567b
takashi_abe <takashi.abe@synergy-effect.jp>
2023-07-27 13:15:58 +0900

    商品詳細の在庫情報

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Stock;
 
 class ItemController extends Controller
 {
@@ -50,7 +51,13 @@ class ItemController extends Controller
     public function show($id)
     {
         $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+        ->sum('quantity');
 
-        return view('user.show', compact('product'));
+        if($quantity > 9){
+            $quantity = 9;
+           }
+
+        return view('user.show', compact('product', 'quantity'));
     }
 }
