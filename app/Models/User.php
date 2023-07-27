8e1354e550f480c012267a1d85086211fd528082
takashi_abe <takashi.abe@synergy-effect.jp>
2023-07-27 13:32:18 +0900

    cart relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+// use Laravel\Sanctum\HasApiTokens;
+use App\Models\Product;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function products()
+        {
+            return $this->belongsToMany(Product::class, 'carts')
+            ->withPivot(['id', 'quantity']); 
+        }
 }
