3493b4651c5230bee9139faa7ef63ac4ee983891
takashi_abe <takashi.abe@synergy-effect.jp>
2023-07-30 15:19:21 +0900

    キュー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -11,6 +11,7 @@ use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -35,8 +36,13 @@ class ItemController extends Controller
     {
         // dd($request);
 
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+        //同期的に送信
+        // Mail::to('test@example.com')
+        // ->send(new TestMail());
+
+        //非同期に送信
+        SendThanksMail::dispatch();
+
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
