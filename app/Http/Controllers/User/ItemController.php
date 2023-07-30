02a160ada676bcd15e0b87730bb8d34a5898f933
takashi_abe <takashi.abe@synergy-effect.jp>
2023-07-30 14:12:42 +0900

    検索フォーム　カテゴリー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -31,12 +31,13 @@ class ItemController extends Controller
     
     public function index(Request $request)
     {
-        dd($request);
+        // dd($request);
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
         $products = Product::availableItems()
+        ->selectCategory($request->category ?? '0')
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
         
